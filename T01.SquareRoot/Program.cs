76ead8d491dd9fa7e1d51bb47e1f4a9573347538
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-03-14 23:57:50 +0200

    Add files via upload

---
+++ T01.SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T01.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
---
+++ T02.EnterNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace T02.EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", ReadNumber(1, 100)));
+        }
+
+        private static List<int> ReadNumber(int start, int end)
+        {
+            List<int> numbers = new List<int>();
+
+            int validNumbers = 0;
+            int i = 1;
+
+            while(validNumbers < 10)
+            {
+                string input = Console.ReadLine();
+
+                try
+                {
+                    if (!int.TryParse(input, out int res))
+                    {
+                        throw new ArgumentException("Invalid Number!");
+                    }
+
+                    if (res <= 1 || res <= i || res >= 100)
+                    {
+                        throw new ArgumentException($"Your number is not in range {i} - 100!");
+                    }
+
+                    if (res > i)
+                    {
+                        i = res;
+                    }
+
+                    validNumbers++;
+                    numbers.Add(res);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return numbers;
+        }
+    }
+}
